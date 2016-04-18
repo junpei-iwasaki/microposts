f852697ebc0f2d7c9e4e10bae008059fd3fe64cf
junpei_iwasaki <junpei-iwasaki@united.jp>
2016-04-18 02:34:50 +0000

    update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,3 +11,20 @@ module SessionsHelper
     session[:forwarding_url] = request.url if request.get?
   end
 end
+
+  # 与えられたユーザーをログイン
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 永続セッションとしてユーザーを記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 与えられたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417125436) do
+ActiveRecord::Schema.define(version: 20160418014712) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160417125436) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.text     "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
