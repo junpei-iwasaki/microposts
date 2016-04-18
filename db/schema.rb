152943d7df03938390ede05088462bd07c444d46
junpei_iwasaki <junpei-iwasaki@united.jp>
2016-04-18 13:56:48 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416063027) do
+ActiveRecord::Schema.define(version: 20160418045610) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "tweet_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "favorites", ["created_at"], name: "index_favorites_on_created_at"
+  add_index "favorites", ["tweet_id"], name: "index_favorites_on_tweet_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,7 +51,8 @@ ActiveRecord::Schema.define(version: 20160416063027) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "area"
+    t.text     "age"
+    t.text     "area"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
